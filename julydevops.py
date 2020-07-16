3a8b7b9f452e26093d06df4e4fcda9ca6e43f62d
Domenico Gagliano <domenico.gagliano1973+github@gmail.com>
2020-07-16 17:04:29 +0100

    Add file julidevops.py as solution for challenge 1

---
+++ julydevops.py
@@ -0,0 +1,10 @@
+cohortFirstNames = ["Christopher","John","Jason","Arsalan","Domenico",
+                    "Joshua","Ryan","Bradley","Javas","Sithembiso","Jacob","Clifford",
+                    "Wasim","Mohamed","Tobias","Christopher","Samuel","Edmund","Amanda",
+                    "Steven","Diarmuid"]
+
+cohortFirstNames.append("Luke")
+
+print(cohortFirstNames[4])
+
+print(cohortFirstNames.count("Christopher"))
