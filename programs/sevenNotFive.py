e14c4d362eb1ab572bb6d1e80657b68ba052d4a1
Domenico Gagliano <domenico.gagliano1973+github@gmail.com>
2020-07-28 08:02:02 +0100

    challenge of the day 27/07/2020 fixed errors

--- programs/sevenNotFive.py
+++ programs/sevenNotFive.py
@@ -1,3 +1,13 @@
+"""
+Write a program to:
+
+find all numbers, between 2000 and 3200 (both included), that are divisible by 7, 
+but are not a multiple of 5
+
+The numbers obtained should be returned on a single line. seperated by commas.
+"""
+
+
 def sevenNotFive():
     result =[]
     for i in range(2000,3201):
@@ -5,4 +15,4 @@ def sevenNotFive():
             result.append(str(i))
     return ",".join(result)
 
-#print(sevenNotFive())
+print("2401" in sevenNotFive())
--- test/test_seveNotFive.py
+++ test/test_seveNotFive.py
@@ -1,6 +1,9 @@
 import pytest
 from programs.sevenNotFive import sevenNotFive
 
-def test_sevenNotFive():
-    for string in sevenNotFive():
-        assert "2100" not in string
+def test_sevenNotFive_one():
+        assert "2100" not in sevenNotFive()
+
+def test_sevenNotFive_two():
+        assert "2401" in sevenNotFive()
+     
